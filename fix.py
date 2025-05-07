3fd7a573de7a3a571bb7d651da4111670914abb8
anukams <anukamstanley@gmail.com>
2025-05-07 20:15:42 +0100

    My first commit for a fix

--- fix.py
+++ fix.py
@@ -1,1 +1,10 @@
-print("These is the method i used to fix all my codes")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Welcome Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
