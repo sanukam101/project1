38b11b317b73471daa3f7d9cd34ebe47108dcf92
anukams <anukamstanley@gmail.com>
2025-05-12 12:07:53 +0100

    third commit

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask to jenkins!"
+    return "Hello from Flask to you my friends!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
